525a6a0f3e6f7a3dce7b9d73ca2ddfd53f49495c
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-20 11:03:26 -0700

    refactored code so functions perform 1 task, changed names to be consistent

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,22 +1,22 @@
-def promptUser 
+def prompt_user 
    # instruct the user to input a string
    puts "What message would you like to encode?"
    # save it as a variable
-   secret_message = gets.chomp 
+   message = gets.chomp 
    # ask the user for a shift value
    puts "Enter a shift value"
    # save as a var
-   shift_value = gets.chomp.to_i
+   value = gets.chomp.to_i
    # pass the string and the shift value into a method
-   encode_message(secret_message, shift_value)
+   return [message, value]
 end
 
-def encode_message(message, value) 
+def encode_message(secret_message, shift_value) 
    # create an array of the alphabet to be used by the cipher
    lowercase_alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
    uppercase_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
    # split the string into an array of letters
-   message_array = message.split("")
+   message_array = secret_message.split("")
    encoded_array = []
    # loop over each letter in the string array
    for letter in message_array
@@ -28,15 +28,14 @@ def encode_message(message, value)
          array_position = uppercase_alphabet.index(letter)
          # if it's not in either letter array, it's a number or a symbol
          if array_position == nil
-            puts "This cipher does not encode numbers or symbols"
-            promptUser()
+            puts "This cipher does not encode numbers or symbols, please try again!"
             return
          end
          isUppercase = true
       end
       # add the shift factor to that index position
-      encoded_position = array_position + value
-      # add logic to check if the new index position will be > 25 
+      encoded_position = array_position + shift_value
+      # handle index positions > 25 
       if encoded_position > 25 
          dmod = encoded_position.divmod(26)
          encoded_position = dmod[1]
@@ -44,8 +43,14 @@ def encode_message(message, value)
       # add that letter to a new array
       isUppercase ? encoded_array.push(uppercase_alphabet[encoded_position]) : encoded_array.push(lowercase_alphabet[encoded_position]) 
    end   
-     # print the output 
-   puts encoded_array.join("")
+   # print the output 
+   return encoded_array.join("")
 end
 
-promptUser()
+def print_message(message)
+   puts message
+end
+
+user_data = prompt_user()
+encoded_message = encode_message(user_data[0], user_data[1])
+print_message(encoded_message)
